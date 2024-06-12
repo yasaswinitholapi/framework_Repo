be18ef27abae8347ef12e0c500eb188ba7c9d8cf
yasaswinitholapi <152694397+yasaswinitholapi@users.noreply.github.com>
2024-06-12 17:51:16 +0530

    first push

---
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,25 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createOrgTest() {
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+
+
+
+	System.out.println("excute createOrgTest");
+}
+@Test
+public void modifyorgTest() {
+	System.out.println("excute modifyorgTest");
+}
+}
---
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+@Test
+public void createcontactTest() {
+	System.out.println("excute createcontact");
+}
+@Test
+public void modifycontactTest() {
+	System.out.println("excute modifycontactTest");
+}
+}
